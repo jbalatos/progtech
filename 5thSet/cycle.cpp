28d4a3af19c82fc4b848a037779e576ebc8cf1a3
Jim Balatos <jbalatos@gmail.com>
2022-05-28 21:04:43 +0300

    Fixed semicolons in cycle.cpp

--- 5thSet/cycle.cpp
+++ 5thSet/cycle.cpp
@@ -7,25 +7,29 @@ class Graph {
 	public:
 		Graph (int v);
 		~Graph();
+
 		void addEdge (int u, int v) noexcept;
+
 		bool cycle (vector<int>& path) const noexcept;
 };
 
 int main() {
-	int V, E;
-	cin >> V >> E;
-	Graph g(V);
-	for (int i = 0; i < E; ++i) {
-		int u, v; cin >> u >> v; g.addEdge(u, v);
+	int V, E;
+	cin >> V >> E;
+	Graph g(V);
+	for (int i = 0; i<E; ++i) {
+		int u, v; cin >> u >> v; g.addEdge(u, v);
 	}
-	vector<int> path;
-	bool c = g.cycle(path);
+	vector<int> path;
+	bool c = g.cycle(path);
 	if (c) {
-		cout << "CYCLE: ";
-			for (int i = 0; i < path.size(); ++i)
-				cout << path[i] << (i == path.size()-1 ? "\n" : " ");
+		cout << "CYCLE: ";
+			for (size_t i = 0; i < path.size(); ++i)
+				cout << path[i] << (i == path.size()-1 ? "\n" : " ");
 	} else {
-		cout << "NO CYCLE" << endl;
+		cout << "NO CYCLE" << endl;
 	}
 	return 0;
 }
+
+
